3552c8b1c3d6dcf85d99d5e1335d0a71be3008f2
Tommaso Meledina <tommaso.meledina@it.ibm.com>
2021-09-11 16:20:20 +0200

    feat: move db connection params to env local env.json currently doesn't work

--- src/handlers/hello-function-2.ts
+++ src/handlers/hello-function-2.ts
@@ -3,21 +3,30 @@ import mariadb = require('mariadb');
 
 const SOME_SQL_QUERY = 'SELECT * FROM PEWPEW_ARENA_APP.CHARACTER';
 
-const connectionPool = mariadb.createPool({
-  host: "host",
-  user: "user",
-  password: "password",
-  connectionLimit: 10    //TODO: not sure how to handle this
-});
+var connectionPool : mariadb.Pool;
+
+const initializeConnectionPool = async () => {
+    console.log(`About to initialize pool, params are: ${process.env.DATABASE_HOST} ${process.env.DATABASE_USER} ${process.env.DATABASE_PASSWORD}`);
+    connectionPool = mariadb.createPool({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        connectionLimit: 10    //TODO: not sure how to handle this
+      });
+}
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
     console.log("About to connect");
+    if(!connectionPool) {
+        console.log(`Connection pool is not initialized, initializing it now`);
+        await initializeConnectionPool();
+    }
     return new Promise((resolve,reject)=>{
         connectionPool.getConnection()
         .then((connection)=>{
             connection.query(SOME_SQL_QUERY)
             .then((result)=>{
-                console.debug(`All is well, got result: ${result}`);
+                console.debug(`All is well, got result: ${JSON.stringify(result)}`);
                 resolve({ body: JSON.stringify({ message: result }), statusCode: 200 });
             })
             .catch((err)=>{
