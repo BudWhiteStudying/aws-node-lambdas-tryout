5cd7776ab0512d20360a26b1c46c52897b8247de
Tommaso Meledina <tommaso.meledina@it.ibm.com>
2021-09-12 11:55:35 +0200

    feat: improve local db connection technique

--- pack-and-deploy.sh
+++ pack-and-deploy.sh
@@ -3,5 +3,5 @@
 MY_BUCKET=node-lambdas-test
 REGION=us-east-1
 MY_STACK_NAME=node-lambdas-test
-echo "About to execute sam build && sam package --output-template-file packaged.yaml --s3-bucket $MY_BUCKET --region $REGION && aws cloudformation deploy --region $REGION --template-file packaged.yaml --stack-name $MY_STACK_NAME --capabilities CAPABILITY_IAM"
-sam build && sam package --output-template-file packaged.yaml --s3-bucket $MY_BUCKET --region $REGION && aws cloudformation deploy --region $REGION --template-file packaged.yaml --stack-name $MY_STACK_NAME --capabilities CAPABILITY_IAM
+echo "About to execute npx webpack && sam package --output-template-file packaged.yaml --s3-bucket $MY_BUCKET --region $REGION && aws cloudformation deploy --region $REGION --template-file packaged.yaml --stack-name $MY_STACK_NAME --capabilities CAPABILITY_IAM"
+npx webpack && sam package --output-template-file packaged.yaml --s3-bucket $MY_BUCKET --region $REGION && aws cloudformation deploy --region $REGION --template-file packaged.yaml --stack-name $MY_STACK_NAME --capabilities CAPABILITY_IAM
